5c86471f1fa8dcdd64995b58a37d87d7df1a2e2b
Kia Martinez <kiamartinez@admins-MBP.home>
2022-01-11 16:03:32 -0600

    updated app to kia

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello Caleb kinsella');
+    res.send('Hello Kia');
 });
 
 app.listen(PORT, HOST);
