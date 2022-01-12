971701dcc47c24be0d9ea0477fed9f6230c0ba3d
Kia Martinez <kiamartinez@admins-MBP.home>
2022-01-12 09:29:49 -0600

    miss my wife test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello Kia');
+    res.send('i miss my wife. very badly.');
 });
 
 app.listen(PORT, HOST);
